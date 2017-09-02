48d338915b0467e2080cea23c229872fa17a0cc4
Jan Raventós <janraven55@gmail.com>
2017-09-02 05:20:37 +0200

    13007 Red-Eyed XaXa

--- common/data/units.js
+++ common/data/units.js
@@ -6,12 +6,20 @@ window.units = [
     [ "Corvatz", "12004", ["Sword", "Neutral"], 2, null, null, null, 100, null, null, null, null, 6613, 125, 1494, null ],
     [ "Thinker", "12005", ["Sword", "Neutral"], 2, null, null, null, 100, null, null, null, null, 6357, 125, 1563, null ],
     [ "Sigurd", "12006", ["Sword", "Wind"], 2, null, null, null, 100, null, null, null, null, 6454, 125, 1555, null ],
-    [ "Klein", "12002", ["Sword", "Neutral"], 2, null, null, null, 100, null, null, null, null, 6262, 132, 1578, null ],
-    [ "Klein", "12002", ["Sword", "Neutral"], 2, null, null, null, 100, null, null, null, null, 6262, 132, 1578, null ],
-    [ "Klein", "12002", ["Sword", "Neutral"], 2, null, null, null, 100, null, null, null, null, 6262, 132, 1578, null ],
+    [ "Kirito [Spriggan]", "13001", ["Sword", "Dark"], 3, null, null, null, 100, null, null, null, null, 8803, 168, 2155, null ],
+    [ "Kirito [Solo]", "13002", ["Sword", "Neutral"], 3, null, null, null, 100, null, null, null, null, 8891, 168, 1957, null ],
+    [ "Klein [Katana Master]", "13003", ["Sword", "Neutral"], 3, null, null, null, 100, null, null, null, null, 8538, 175, 2188, null ],
+    [ "Klein [Salamander Samurai]", "13004", ["Sword", "Fire"], 3, null, null, null, 100, null, null, null, null, 8627, 175, 2017, null ],
+    [ "Sumeragi", "13005", ["Sword", "Water"], 3, null, null, null, 100, null, null, null, null, 8407, 168, 2056, null ],
+    [ "Heathcliff [Guild Commander]", "13006", ["Sword", "Holy"], 3, null, null, null, 100, null, null, null, null, 9066, 159, 2081, null ],
+    [ "Red-Eyed XaXa", "13007", ["Sword", "Neutral"], 3, null, null, null, 100, null, null, null, null, 8979, 159, 2070, null ],
+    [ "Name", "ID", ["WEAPON", "ELEMENT"], rarity, null, null, null, 100, null, null, null, null, HP, MP, ATK, null ],
+    [ "Name", "ID", ["WEAPON", "ELEMENT"], rarity, null, null, null, 100, null, null, null, null, HP, MP, ATK, null ],
     [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
     ];
     
 // [ "Name", "Type", [ "Class1", "Class2" ], null, null, null, null, null, null, null, null, null, null, null, null, null ],
 // or [ "Name", "Type", "Class1", null, null, null, null, null, null, null, null, null, null, null, null, null ],
 // [ "Name", "Type", [ "Class1", "Class2" ], Stars, Cost, Combo, sockets, maxLVL, EXPToMax, lvl1HP, lvl1ATK, lvl1RCV, MAXHP, MAXATK, MAXRCV, Growth Rate ],
+
+//    [ "Name", "ID", ["WEAPON", "ELEMENT"], rarity, null, null, null, 100, null, null, null, null, HP, MP, ATK, null ],
