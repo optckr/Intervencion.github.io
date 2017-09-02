12b2d7b048ffb2f45a5a57b0e4a2906cfc173175
Jan Raventós <janraven55@gmail.com>
2017-09-02 05:36:19 +0200

    Just work. Please.

--- common/data/units.js
+++ common/data/units.js
@@ -1,5 +1,5 @@
 window.units = [
-//    [ "Kirito", "11001", ["Sword", "Neutral"], 1, 0, 0, 0, 100, 1, 0, 0, 0, 5331, 118, 1129, 1 ],
+//    [ "Kirito", "11001", ["Sword", "Neutral"], 1, null, null, null, 100, null, null, null, null, 5331, 118, 1129, 1 ],
 //    [ "Kirito [Former Beta Tester]", "12001", ["Sword", "Neutral"], 2, 0, 0, 0, 100, 1, 0, 0, 0, 6390, 132, 1548, 1 ],
     [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
     ];
