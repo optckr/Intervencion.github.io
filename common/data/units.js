d21766dcc40fd388c8ab2c4ce705b116b326dfb3
Jan Raventós <janraven55@gmail.com>
2017-09-02 05:38:02 +0200

    ...

--- common/data/units.js
+++ common/data/units.js
@@ -1,9 +1,9 @@
 window.units = [
 //    [ "Kirito", "11001", ["Sword", "Neutral"], 1, null, null, null, 100, null, null, null, null, 5331, 118, 1129, 1 ],
 //    [ "Kirito [Former Beta Tester]", "12001", ["Sword", "Neutral"], 2, 0, 0, 0, 100, 1, 0, 0, 0, 6390, 132, 1548, 1 ],
-    [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
-    [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
-    [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
+	[ "Monkey D. Luffy", "STR", "Fighter", 2, 1, 5, 0, 5, 163, 42, 15, 8, 134, 68, 15, 1 ],
+    [ "Monkey D. Luffy Gum-Gum Pistol", "STR", "Fighter", 3, 4, 6, 0, 20, 26184, 130, 76, 14, 510, 249, 49, 0.7 ],
+    [ "Monkey D. Luffy Gum-Gum Bazooka", "STR", "Fighter", 4, 7, 6, 2, 35, 136167, 404, 227, 39, 975, 522, 105, 1 ],
     [ "Monkey D. Luffy Gear 2", "STR", [ "Fighter", "Free Spirit" ], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
     ];
     
