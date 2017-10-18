8a6ba1dde7a62dc363b449ada38de661b25aa7f9
Jan Raventós <janraven55@gmail.com>
2017-10-19 01:04:20 +0200

    Maybe?

--- common/data/effects.js
+++ common/data/effects.js
@@ -402,5 +402,35 @@ window.effects = {
         id: 42, // don't change this
         chainLimiter: function() { return 1.5; }
     },
+
+    'Treasure Map Mihawk - Buggy & Zoro 2.5x stats boost; Lucy6* 1.2x': {
+        description: 'Boost stats of certain characters',
+        thumb: 1808,
+        id: 43, // don't change this
+        atk: function(p) { 
+                            var buggy = 1.0, zoro = 1.0, lucy = 1.0;
+                            if(p.unit.is('1806')) { boost1 = 2.5}
+                            if(p.unit.is('1804')) { boost2 = 2.5}
+                            if(p.unit.is('1794')) { boost2 = 1.2}
+                            //console.log(boost1+" "+boost2+" "+boost3+" "+boost4+" "+boost5)
+                            return 1*buggy*zoro*lucy; 
+                        },
+        hp: function(p) { 
+                            var buggy = 1.0, zoro = 1.0, lucy = 1.0;
+                            if(p.unit.is('1806')) { boost1 = 2.5}
+                            if(p.unit.is('1804')) { boost2 = 2.5}
+                            if(p.unit.is('1794')) { boost2 = 1.2}
+                            //console.log(boost1+" "+boost2+" "+boost3+" "+boost4+" "+boost5)
+                            return 1*buggy*zoro*lucy; 
+                        },
+        rcv: function(p) { 
+                            var buggy = 1.0, zoro = 1.0, lucy = 1.0;
+                            if(p.unit.is('1806')) { boost1 = 2.5}
+                            if(p.unit.is('1804')) { boost2 = 2.5}
+                            if(p.unit.is('1794')) { boost2 = 1.2}
+                            //console.log(boost1+" "+boost2+" "+boost3+" "+boost4+" "+boost5)
+                            return 1*buggy*zoro*lucy; 
+                        },
+    },
     
 };
