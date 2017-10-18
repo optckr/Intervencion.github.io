459881f37e6c6576fa11535d809fc06d00086ebd
Jan Raventós <janraven55@gmail.com>
2017-10-18 04:01:15 +0200

    ostia

--- common/data/captains.js
+++ common/data/captains.js
@@ -5562,10 +5562,15 @@ window.captains = {
     },
     1808: {
         atk: function(p){ 
-            return !p.unit.class.has("Slasher") ? 1 : ((window.specials[1807].turnedOn || window.specials[1808].turnedOn) ? 3.84 : 2.5); },
+            var specialEnabled = false;
+            for(var i=0;i<2.5;i++)
+            {
+                if(window.specials[1807].turnedOn[i]==true){specialEnabled = true;}
+                if(window.specials[1808].turnedOn[i]==true){specialEnabled = true;}
+            }
+            return specialEnabled ? 3.84 : 2.5; },
         hp: function(p) { return 1.3 }
-    },
-    1809: {
+    },9: {
         hitAtk: function(p) {
             return p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect"]) ? 2.25 : 1;
         },
