c57ee36aa13d644a879bcedaef51f84d2c5b20c7
Jan Raventós <janraven55@gmail.com>
2017-10-18 04:07:55 +0200

    CA
    Using CA action

--- common/data/captains.js
+++ common/data/captains.js
@@ -5549,17 +5549,17 @@ window.captains = {
         atk: function(p) { return p.unit.cost <= 30 ? 2.25 : 1; },
         hp: function(p) { return p.unit.cost <= 30 ? 1.2 : 1; }
     },
-    1807: {
-        atk: function(p){ 
-            var specialEnabled = false;
-            for(var i=0;i<2.5;i++)
-            {
-                if(window.specials[1807].turnedOn[i]==true){specialEnabled = true;}
-                if(window.specials[1808].turnedOn[i]==true){specialEnabled = true;}
-            }
-            return specialEnabled ? 3.84 : 2.5; },
-        hp: function(p) { return 1.3 }
-    },
+//    1807: {
+//        atk: function(p){ 
+//            var specialEnabled = false;
+//            for(var i=0;i<2.5;i++)
+//            {
+//                if(window.specials[1807].turnedOn[i]==true){specialEnabled = true;}
+//                if(window.specials[1808].turnedOn[i]==true){specialEnabled = true;}
+//            }
+//            return specialEnabled ? 3.84 : 2.5; },
+//        hp: function(p) { return 1.3 }
+//    },
 //    1808: {
 //        atk: function(p){ 
 //            var specialEnabled = false;
@@ -5571,6 +5571,12 @@ window.captains = {
 //            return specialEnabled ? 3.84 : 2.5; },
 //        hp: function(p) { return 1.3 }
 //    },
+    1807: {
+        atk: function(p){ 
+            return p.unit.class.has("Slasher") ? p.actions[p.sourceSlot] ? 3.5 : 2.5 : 1;
+        },
+        hp: function(p) { return 1.3 }
+    },
     1808: {
         atk: function(p){ 
             return p.unit.class.has("Slasher") ? p.actions[p.sourceSlot] ? 3.5 : 2.5 : 1;
