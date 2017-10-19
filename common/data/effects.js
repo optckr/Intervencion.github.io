70a2e0375b590861606512d39f2e5c0d2b723dd7
Jan Raventós <janraven55@gmail.com>
2017-10-19 06:05:29 +0200

    Still

--- common/data/effects.js
+++ common/data/effects.js
@@ -565,13 +565,13 @@ window.effects = {
         },
         hp: function(p) {
             var id = p.number + 1,
-                matching = ((id > 1742 && id < 1745) ||
+                matching = ((id > 1740 && id < 1745) ||
                     [ 1795, 1796, 1539, 1540, 1823, 1844, 1277, 1278 ].indexOf(id) != -1);
             return !matching ? 1 : 1.75;
         },
         rcv: function(p) {
             var id = p.number + 1,
-                matching = ((id > 1742 && id < 1745) ||
+                matching = ((id > 1740 && id < 1745) ||
                     [ 1795, 1796, 1539, 1540, 1823, 1844, 1277, 1278 ].indexOf(id) != -1);
             return !matching ? 1 : 1.75;
         },
